870875b5c611c9794da270295350f698cb46bd8c
Washington Antunes <washington.cead@gmail.com>
2022-07-15 10:20:23 -0300

    API test

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -21,6 +21,9 @@ public class Sale {
 	private Double amount;
 	private LocalDate date;
 	
+	public Sale() {
+	}
+	
 	public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
 		this.id = id;
 		this.sellerName = sellerName;
